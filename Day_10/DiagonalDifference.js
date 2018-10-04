9e2f6771e3bd448a3d4c543008e0d984c3da37b7
Bharvi Bissa <bharvi.b@techaspect.com>
2018-10-04 12:31:05 +0530

    Added Source Code for Challenge DiagonalDifference

---
+++ Day_10/DiagonalDifference.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the diagonalDifference function below.
+function diagonalDifference(arr) {
+    var d1=0;
+    var d2=0;
+    
+    for(var i=0;i<arr.length;i++){
+        d1=d1+arr[i][i];
+    }
+    //System.out.println(d1);
+
+    for(var i=0;i<arr.length;i++){
+        d2=d2+arr[i][arr.length-(i+1)];
+    }
+    //System.out.println(d2);
+
+    return Math.abs(d1-d2);
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
