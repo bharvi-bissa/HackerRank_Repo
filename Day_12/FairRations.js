2f58235059e2007ca21007c3fa679b2a7f7b922c
Bharvi Bissa <bharvi.b@tadigital.com>
2018-10-09 12:06:47 +0530

    Added Source Code For Challenge FairRations

---
+++ Day_12/FairRations.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the fairRations function below.
+function fairRations(B) {
+   
+    var sum = 0;
+    var count = 0;
+
+    for(var i=0; i < B.length; i++){
+        sum += B[i];
+
+        if (sum % 2 == 1) {
+            count += 2;
+        }
+    }
+
+    return (sum % 2 == 0 ? count : "NO");
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const N = parseInt(readLine(), 10);
+
+    const B = readLine().split(' ').map(BTemp => parseInt(BTemp, 10));
+
+    let result = fairRations(B);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
